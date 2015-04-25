4a98b97e5aeda56cab045eb7fc8fb04510685155
wotozone <xoduszzong@gmail.com>
2015-04-25 10:17:17 -0700

    sound update 1

--- src/SoundPack/SoundLoader.java
+++ src/SoundPack/SoundLoader.java
@@ -5,10 +5,33 @@
  */
 package SoundPack;
 
+import javafx.scene.media.Media;
+
 /**
  *
  * @author minjikim
  */
 public class SoundLoader {
+    
+    //BACKGROUND MUSIC
+    public final Media BACKGROUND;
+    
+    //COMBO SOUNDS
+    public final Media COMBO1;
+    public final Media COMBO2;
+    public final Media COMBO3;
+    
+    //WOOHOO
+    public final Media WOOHOO;
+    
+    public SoundLoader(){
+        BACKGROUND= new Media("Sounds/BACKGROUND.mp3");
+        
+        COMBO1= new Media("Sounds/COMBO1.mp3");
+        COMBO2= new Media("Sounds/COMBO2.mp3");
+        COMBO3= new Media("Sounds/COMBO3.mp3");
+        
+        WOOHOO= new Media("Sounds/WOOHOO.mp3");
+    }
     
 }
--- src/SoundPack/SoundManager.java
+++ src/SoundPack/SoundManager.java
@@ -5,10 +5,41 @@
  */
 package SoundPack;
 
+import javafx.scene.media.Media;
+
 /**
  *
  * @author minjikim
  */
 public class SoundManager {
     
+    public SoundLoader soundLoader;
+    
+    public SoundManager(){
+        soundLoader=new SoundLoader();
+    }
+    
+    public Media getComboSound(int value){
+        switch(value){
+            case(0):return soundLoader.COMBO1;
+            case(1):return soundLoader.COMBO2;
+            case(2):return soundLoader.COMBO3;
+            default:
+                System.out.println("No Sound");
+                return soundLoader.COMBO1;
+        }
+        
+    }
+    
+    public Media getBackgroundImage(){
+        return soundLoader.BACKGROUND;
+    }
+    
+    public Media getWooHoo(){
+        return soundLoader.WOOHOO;
+    }
+    
+    //public Media getGameOverImage(){
+    //    return imageLoader.GAME_OVER;
+    //}
 }
